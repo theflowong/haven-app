09f61ad2de472b487047d1e0815e576bd5607be7
Daniel James <danielcolinjames@gmail.com>
2017-10-28 16:45:07 -0700

    Add basic hard-coded "lights all blue" function
    Connect the web interface to the smart lights using Philips Hue API and modified sample code.
    
    When clicking the Start Experience button on index.html, main.js now sets all Philips Hue lights on the local network to the colour blue.
    
    When clicking Stop Experience, it now turns all the lights off.

--- main.js
+++ main.js
@@ -3,12 +3,30 @@ $( document ).ready(function() {
   	var audio = new Audio('audio/sample-audio.mp3');
 
 
-  	function startExperience(){
-  		// ADD HUE API STUFF HERE
+  	function startExperience(){        
+        $.ajax({
+            url: 'http://192.168.86.31/api/EQMB5CwjE2hYxWK4a-lqCvk9pKN5VoNoTvEACoDz/groups/0/action',
+            type: 'PUT',
+//            this one makes a nice simple colour loop but we probably won't need to use it
+//            data: '{"on":true,"bri":255,"sat":255,"hue":46920, "effect":"colorloop"}',
+//            blue
+            data: '{"on":true,"bri":255,"sat":255,"hue":46920}',
+            success: function () {
+            }
+        });
+        
   		audio.play();
   	}
 
   	function stopExperience(){
+        $.ajax({
+            url: 'http://192.168.86.31/api/EQMB5CwjE2hYxWK4a-lqCvk9pKN5VoNoTvEACoDz/groups/0/action',
+            type: 'PUT',
+            data: '{"on":false}',
+            success: function () {
+            }
+        });
+        
   		audio.pause();
   	}
 
