7961a98fc247d13f306f1a53a9a64e952ad40759
Flo Wong <florencewong1020@gmail.com>
2017-10-29 16:56:27 -0700

    Add url for flo's room
    To test with Flo's room, use specified IP address and username

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 $( document ).ready(function() {
-  
+
   	var audio = new Audio('audio/sample-audio.mp3');
 
 
-  	function startExperience(){        
+  	function startExperience(){
         $.ajax({
             url: 'http://192.168.86.31/api/EQMB5CwjE2hYxWK4a-lqCvk9pKN5VoNoTvEACoDz/groups/0/action',
+//            Flo's room:
+//            url: 'http://192.168.0.182/api/P5L62etgqpkSUikcmdABFSCox4cOoyk7SIMefkO3'
             type: 'PUT',
 //            this one makes a nice simple colour loop but we probably won't need to use it
 //            data: '{"on":true,"bri":255,"sat":255,"hue":46920, "effect":"colorloop"}',
@@ -14,7 +16,7 @@ $( document ).ready(function() {
             success: function () {
             }
         });
-        
+
   		audio.play();
   	}
 
@@ -26,7 +28,7 @@ $( document ).ready(function() {
             success: function () {
             }
         });
-        
+
   		audio.pause();
   	}
 
