84fc01fa69c5e026d1a9d1eeeb75629fe6bac5af
Flo Wong <florencewong1020@gmail.com>
2017-10-29 19:18:22 -0700

    Create variables for url ip address
    For easier access/change and to prevent repetitions

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 $( document ).ready(function() {
 
   	var audio = new Audio('audio/sample-audio.mp3');
-
+    var url_ip = 'http://192.168.0.182/api/P5L62etgqpkSUikcmdABFSCox4cOoyk7SIMefkO3'
+      // Flo's room: 'http://192.168.0.182/api/P5L62etgqpkSUikcmdABFSCox4cOoyk7SIMefkO3',
+      // Daniel's room: 'http://192.168.86.31/api/EQMB5CwjE2hYxWK4a-lqCvk9pKN5VoNoTvEACoDz'
+    var url_lights = '/groups/0/action';
+      // or /lights/3/state (depending on light)
 
   	function startExperience(){
+
         $.ajax({
-            url: 'http://192.168.86.31/api/EQMB5CwjE2hYxWK4a-lqCvk9pKN5VoNoTvEACoDz/groups/0/action',
-//            Flo's room:
-//            url: 'http://192.168.0.182/api/P5L62etgqpkSUikcmdABFSCox4cOoyk7SIMefkO3'
+            url: url_ip+url_lights,
             type: 'PUT',
 //            this one makes a nice simple colour loop but we probably won't need to use it
 //            data: '{"on":true,"bri":255,"sat":255,"hue":46920, "effect":"colorloop"}',
@@ -16,13 +19,13 @@ $( document ).ready(function() {
             success: function () {
             }
         });
-
+        
   		audio.play();
   	}
 
   	function stopExperience(){
         $.ajax({
-            url: 'http://192.168.86.31/api/EQMB5CwjE2hYxWK4a-lqCvk9pKN5VoNoTvEACoDz/groups/0/action',
+            url: url_ip+url_lights,
             type: 'PUT',
             data: '{"on":false}',
             success: function () {
